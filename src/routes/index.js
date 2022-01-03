844b04098d0ed5c4d1db9a6453b7f485cdada7f9
ridwanullahgold <belloridwan60@gmail.com>
2022-01-03 13:01:34 +0100

    Creating boilerplate for Demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,5 @@ const userController = require("../controllers/user");
 const checkAuth = require("../middleware/checkAuth");
 
 // User Routes
-router.post("/users/:coin", userController.createUser);
 
 module.exports = router;
